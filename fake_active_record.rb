493e71c1426a4fadbd4b8f7816ecbdae8c49bdfa
Jeffrey Gisin <jgisin@gmail.com>
2016-02-01 16:56:57 -0700

    almost done with create:

--- fake_active_record.rb
+++ fake_active_record.rb
@@ -31,16 +31,75 @@ module FakeActiveRecord
       schema.keys
     end
 
+    def self.all
+      DB.execute("SELECT * from #{self.table_name}")
+    end
 
+    def self.find(id)
+      if id.is_a?(Array)
+        id_list = id.join(",")
+        DB.execute("SELECT * FROM #{self.table_name} WHERE id IN (#{id_list})")
+      else
+        DB.execute("SELECT * FROM #{self.table_name} WHERE id = #{id}")
+      end
+    end
 
+    def self.first
+      DB.execute("SELECT * FROM #{self.table_name} LIMIT 1")
+    end
 
+    def self.last
+      DB.execute("SELECT * FROM #{self.table_name} ORDER BY id DESC LIMIT 1")
+    end
 
-    # YOUR CODE GOES HERE
+    def self.select(column, *others)
+      raise ArgumentError unless self.columns.include?(column)
+      columns = "SELECT #{column}"
+      others.each do |col|
+        raise ArgumentError unless self.columns.include?(col)
+        columns += ", #{col}"
+      end
+      DB.execute(columns + " FROM #{self.table_name}")
+    end
 
+    def self.count
+      DB.execute("SELECT count(*) FROM #{self.table_name}")
+    end
 
+    def self.where(hash)
+      query_arr = []
+      hash.each do |k,v|
+        raise ArgumentError unless self.columns.include?(k)
+        query_arr << "#{k} = '#{v}'"
+      end
+      DB.execute("SELECT * FROM #{self.table_name} WHERE " + query_arr.join(" AND "))
+    end
 
+    def self.create(array_or_hash)
+      column_arr = []
+      value_arr = []
 
+      if array_or_hash.is_a?(Hash)
+        array_or_hash.each do |k,v|
+          raise ArgumentError unless self.columns.include?(k)
+          column_arr << k
+          value_arr << v
+        end
+        DB.execute("INSERT INTO #{self.table_name} (#{column_arr.join(',')}) VALUES (#{value_arr.join(',')}) ")
+
+      elsif array_or_hash.is_a?(Array)
+        array_or_hash.each do |hash|
+          column_arr = []
+          value_arr = []
+          hash.each do |k,v|
+            raise ArgumentError unless self.columns.include?(k)
+            column_arr << k
+            value_arr << "'#{v}'"
+          end
+          DB.execute("INSERT INTO #{self.table_name} (#{column_arr.join(',')}) VALUES (#{value_arr.join(',')}) ")
+        end
+      end
+    end
 
   end
 end
-
